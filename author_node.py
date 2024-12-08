f916fcdd5ff3abff607b93b82bb3827ecaed19c2
EsrefDesign <esrefdesign@gmail.com>
2024-12-09 01:29:43 +0300

    `Added author ID and co-authors to Essay class, updated Essay and Author classes, and modified main.py to accommodate changes.`

--- author_node.py
+++ author_node.py
@@ -1,7 +1,18 @@
 class Author:
-    def __init__(self,name,next=None):
+    def __init__(self,Id,name,next=None):
         self.name=name
         self.next=next
+        self.Id = Id
 
     def __str__(self):
         return str(self.name)
+    
+    def getId(self):
+        if self.Id is None:
+            return 
+        return self.Id
+    
+    def getName(self):
+        if self.Id is None:
+            return 
+        return self.Id
--- essay_node.py
+++ essay_node.py
@@ -1,8 +1,10 @@
 class Essay:
-    def __init__(self,ID,title,next=None):
+    def __init__(self,ID,title,coAuthors,author,next=None):
         self.ID=ID
         self.title=title
         self.next=next
+        self.coAuthors = coAuthors
+        self.author = author
 
     def __str__(self):
         return str(self.name)
@@ -11,12 +13,22 @@ class Essay:
         if self.ID ==None:
             print("Error Code is 1 :The Id Couldn't Found")
             return
-        print(self.ID)
+        return self.ID
     
     def getTitle(self):
         if self.title ==None:
             print("Error Code is 2 :The Title Couldn't Found")
             return
         return self.title
+    
+    def getAuthors(self):
+        if self.coAuthors is None:
+            return 
+        return self.coAuthors
+    
+    def getAuthors(self):
+        if self.author is None:
+            return 
+        return self.author
     
     
