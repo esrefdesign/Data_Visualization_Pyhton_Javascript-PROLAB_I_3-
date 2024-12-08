77e81532cdc2428bb9fc8eaeb89134d373d1ab60
EsrefDesign <esrefdesign@gmail.com>
2024-12-08 16:27:56 +0300

    Learned how to create essay node with excel

--- essay_node.py
+++ essay_node.py
@@ -1,7 +1,22 @@
 class Essay:
-    def __init__(self,name,next=None):
-        self.name=name
+    def __init__(self,ID,title,next=None):
+        self.ID=ID
+        self.title=title
         self.next=next
 
     def __str__(self):
         return str(self.name)
+    
+    def getId(self):
+        if self.ID ==None:
+            print("Error Code is 1 :The Id Couldn't Found")
+            return
+        print(self.ID)
+    
+    def getTitle(self):
+        if self.title ==None:
+            print("Error Code is 2 :The Title Couldn't Found")
+            return
+        return self.title
+    
+    
