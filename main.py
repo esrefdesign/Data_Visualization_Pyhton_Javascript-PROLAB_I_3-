b8f080c8b6e483a12f13e84e40948cfb25f9a927
EsrefDesign <esrefdesign@gmail.com>
2024-12-12 16:20:07 +0300

    `Updated essay_node.py and main.py to reflect changes in Essay class and data processing`

--- main.py
+++ main.py
@@ -4,29 +4,27 @@ from essay_node import Essay
 from bstNode import BST,BSTNode
 from priorityQueue import PriorityQueue
 from graph import Graph
-from collections import Counter
 #import All neededs
 
 
 # Excel'den veriyi çekme
 
-data = pd.read_excel('DATASET.xlsx')
-
-
-
+data = pd.read_excel('DATASET.xlsx',nrows=400)
 
 head = None
 current = None
+
 authors = data['author_name']
+essays = data[['doi','paper_title']].drop_duplicates()
 
 author_to_id = {author: idx for idx, author in enumerate(set(authors))}
 
 # Her yazarı bir kez yazdırma
 unique_authors = [{'Author': author, 'Author_ID': author_to_id[author]} for author in author_to_id]
+unique_essay = [{'Essay_ID': row['doi'], 'Essay_Title': row['paper_title']} for _, row in essays.iterrows()]
 
-# Sonuçları yazdırma
-for entry in unique_authors:
-    new_author= Author(entry['Author'],entry['Author_ID'])
+for entry in unique_essay:
+    new_author= Essay(entry['Essay_ID'],entry['Essay_Title'])
     if head is None:
         head = new_author # Set the head of the linked list
         current = head
@@ -35,25 +33,15 @@ for entry in unique_authors:
         current = new_author # Move to the new essay
     print(f"{current.name}    {current.Id}")
 
-    
 
+# Sonuçları yazdırma
+for entry in unique_authors:
+    new_author= Author(entry['Author_ID'],entry['Author'])
+    if head is None:
+        head = new_author # Set the head of the linked list
+        current = head
+    else:
+        current.next = new_author  # Link the new essay
+        current = new_author # Move to the new essay
+    print(f"{current.name}    {current.Id}")
 
-# Create the linked list
-# for index, row in data.iterrows():
-#     essay_id = row['doi']
-#     essay_title = row['paper_title']
-#     essay_authors = row['coauthors']
-#     essay_author = row['author_name']
-    
-#     new_essay = Author(essay_id, essay_title,essay_authors)
-
-#     if head is None:
-#         head = new_essay  # Set the head of the linked list
-#         current = head
-#     else:
-#         current.next = new_essay  # Link the new essay
-#         current = new_essay  # Move to the new essay
-
-# while head:
-#     print(head.getAuthors())
-#     head = head.next
